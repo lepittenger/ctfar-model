8905bb7e3abbf3935853649f000546904d5ce180
Lauren Pittenger <lepittenger@gmail.com>
2021-04-13 14:44:02 -0400

    Adds ability to delete a model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import ModelsList from './components/ModelsList';
-import ModelForm from './components/ModelForm';
+import AddModelForm from './components/AddModelForm';
 import React from 'react';
+import base from './base';
 
 class App extends React.Component {
   state = {
     models: {}
   };
+  componentDidMount() {
+    this.ref = base.syncState(`${this.props.match.params.userId}/models`, {
+      context: this,
+      state: 'models'
+    });
+  }
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
   addModel = (model) => {
     // 1. take a copy of the existing state bc you don't want to reach into state and modify it directly
     const models = {...this.state.models}
@@ -17,6 +27,15 @@ class App extends React.Component {
       models: models
     });
   };
+  deleteModel = (key) => {
+    // 1. take a copy of state
+    const models = { ...this.state.models };
+    console.log(models[key]);
+    // 2. update the state
+    models[key] = null;
+    // 3.  update state
+    this.setState({ models });
+  };
   render() {
     return (
       <div className="App">
@@ -24,8 +43,13 @@ class App extends React.Component {
           <p>The CTFAR Model</p>
         </header>
         <div className="models-wrap">
-          <ModelsList models={this.state.models} />
-          <ModelForm addModel={this.addModel} />
+          <ModelsList 
+            models={this.state.models}
+            deleteModel={this.deleteModel}
+          />
+          <AddModelForm 
+            addModel={this.addModel} 
+          />
         </div>
       </div>
     )
--- src/components/ModelExcerpt.js
+++ src/components/ModelExcerpt.js
@@ -4,7 +4,7 @@ class ModelExcerpt extends React.Component {
     render() {
         return (
             <li>
-                <h4><span className="date">4/12/21</span> {this.props.details.titleRef}</h4>
+                <h4><span className="date">4/12/21</span> {this.props.model.titleRef ? this.props.model.titleRef : 'Untitled'} <button className="deleteModel" onClick={() => this.props.deleteModel(this.props.index)}>&times;</button></h4>
             </li>
         )
     }
