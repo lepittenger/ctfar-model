527ace081ad5d324e7384f123ab3eb6a93480389
Lauren Pittenger <lepittenger@gmail.com>
2021-04-13 09:43:28 -0400

    Adds a count of total models

--- src/components/ModelsList.js
+++ src/components/ModelsList.js
@@ -3,9 +3,11 @@ import ModelExcerpt from "./ModelExcerpt";
 
 class ModelsList extends React.Component {
     render() {
+        const modelsIds = Object.keys(this.props.models);
+        const modelsCount = modelsIds.length;
         return (
             <div className="models-list">
-                <h3>Models</h3>
+                <h3>Models <span className="modelsCount">({modelsCount})</span> <button className="addNewModel">&#43;</button></h3>
                 <ul>
                     {Object.keys(this.props.models).map(key => <ModelExcerpt key={key}details={this.props.models[key]} />)}
                 </ul>
